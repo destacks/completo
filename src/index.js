10a9913189f8286ed7b97b10bc4348502d91d2fa
Alexis Michaltsis <a.michaltsis@gmail.com>
2021-03-04 18:35:24 +0100

    Show items list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 function completo(element, target) {
+  const list = document.createElement("DIV");
+  list.setAttribute("id", `${element.id}-list`);
+  element.parentNode.appendChild(list);
+
   element.addEventListener("click", function () {
     console.log("Click");
   });
@@ -7,14 +11,24 @@ function completo(element, target) {
     fetchFromTarget(element.value, target);
   });
 
+  function createListItems(result) {
+    let listItems = "";
+    for (let i = 0; i < result.length; i++) {
+      listItems = `${listItems}<div>${result[i]}</div>`;
+    }
+    return listItems;
+  }
+
   function fetchFromTarget(value, target) {
     fetch(`${target}?query=${value}`)
       .then((response) => response.json())
       .then((data) => {
         if (Array.isArray(data) && data.length) {
-            console.log(data[0]["result"]);
+          const result = data[0]["result"];
+          list.innerHTML = createListItems(result);
         } else {
-            console.log([]);
+          const result = [];
+          list.innerHTML = createListItems(result);
         }
       });
   }
